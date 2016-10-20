a9f45e49dd847d1fa43c2352e1ab2ddf5abc4f83
Cristian Buffa <bufface@gmail.com>
2016-10-20 15:27:47 -0300

    Video 65: Mostrar GIF

--- src/messenger-elements/messenger-giphy/messenger-giphy.js
+++ src/messenger-elements/messenger-giphy/messenger-giphy.js
@@ -1,24 +1,22 @@
 class MGiphy {
   constructor(keyword) {
-    this.keyword = keybord
+    this.keyword = keyword
     this.endpoint = "http://api.giphy.com/v1/gifs"
-    this.api_key = "dc6zaT0xFJmzC"
-    this.type = "transalate"
+    this.api_key = "dc6zaTOxFJmzC"
+    this.type = "translate"
   }
 
   getGifUrl(callback) {
     var xhr = new XMLHttpRequest()
     xhr.open("GET", this.endpoint + "/" + this.type + "?s=" + this.keyword + "&api_key=" + this.api_key)
     xhr.responseType = "json"
-
-    xhr.onLoad = function () {
-      callback(this.response.data.original.mp4)
+    xhr.onload = function () {
+      callback(this.response.data.images.original.mp4)
     }
-
     xhr.send()
   }
 
   static getUrlAsync(keyword, callback) {
-    new MGiphy(keyword)
+    new MGiphy(keyword).getGifUrl(callback)
   }
 }
